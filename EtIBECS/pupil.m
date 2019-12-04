f2bf990fe9283af3e65c50e4c8652d3738d69c75
AprilMyers <50928407+AprilMyers@users.noreply.github.com>
2019-12-04 15:50:26 -0800

    Add files via upload

--- EtIBECS/pupil.m
+++ EtIBECS/pupil.m
@@ -1,6 +1,66 @@
-function pupil(inputArg1,inputArg2)
-%PUPIL Summary of this function goes here
-%   Detailed explanation goes here
+% function pupil(inputArg1,inputArg2)
+% %PUPIL Summary of this function goes here
+% %   Detailed explanation goes here
+% 
+%     % Call Setup and File Read In Function
+%     [nTrls, dFldr, cFNames, aTrl, sTrl, nCam1, nCam2, nLick, nMWhl,... 
+%         nTrig, nValv, nShck, nMove, nProt, nStim, fSesh, ... 
+%         fCam1, fCam2, fLick, fMWhl, fTrig, fValv, fShck, ...
+%         fMove, fProt, fStim, fTxt, fTiff, fSTrl,seshTitle] = setUpPupilAnalysis();
+%     
+%     %Ryans Figure Settings to Display Figures
+%     RyansFigureSettings;
+%     
+%     
+%     % Filter files based on name attributes
+%     iTOI = find(fSesh & fCam1 & fSTrl & fTiff);
+%         
+%     % Call Cropping function
+%     [minX, maxX, minY, maxY] = selectCroppedRegionPupilCenterMethod(cFNames, iTOI, dFldr);
+%         
+%     % Call Pupil Analysis function on each trial
+%     tic
+%     pupilSizes = {};
+%     for u = 1:1 %1:nTrls
+%         [pupilSizeTrialU, pupilVids] = pupilAnalysis(dFldr, u, iTOI, cFNames, minX, maxX, minY, maxY);
+%         pupilSizes{u} = pupilSizeTrialU;
+%         disp("Done with trial");
+%         disp(u);
+%     end
+%     toc
+%     
+%     % Converts the cell array to a Matrix of all values and use Nans to pad
+%     disp("The dimension of pupilSizes is: ")
+%     pupilSizes
+%     
+% %     [pupilSizesMat] = convertCellArraytoMat(pupilSizes);
+% %     
+% %     % Create the dt matrix
+% %     [pupilSizedt] = creating_dt(pupilSizes);
+% %     
+% %     % Filter the dt array
+% %     [pupilareaProc, pupilsize_filtered] = filterMatrix(pupilSizes, pupilSizedt);
+% %     
+% %     % Converts the cell array to a matrix of all values and use Nans to pad
+% %     [pupilsize_filtered_mat] = convertCellArraytoMat(pupilsize_filtered);
+% %     
+% %     % Interpolation
+% %     [pupilsize_filtered] = interpolation(pupilsize_filtered);
+% %     
+% %      % Converts the cell array to a matrix of all values and use Nans to pad
+% %     [pupilsize_interp_mat] = convertCellArraytoMat(pupilsize_filtered);
+%     
+% end
+
+
+
+function [cFNames iTOI, pupilS] = pupil(inputArg1,inputArg2)
+%PUPIL: Outputs a structure containing 4 fields
+% 1) Area
+% 2) MajorAxisLength 
+% 3) xPosition
+% 4) yPosition
+
 
     % Call Setup and File Read In Function
     [nTrls, dFldr, cFNames, aTrl, sTrl, nCam1, nCam2, nLick, nMWhl,... 
@@ -20,31 +80,42 @@ function pupil(inputArg1,inputArg2)
         
     % Call Pupil Analysis function on each trial
     tic
-    pupilSizes = {};
-    for u = 1:1 %1:nTrls
+    disp("This is the number of trials: ")
+    disp(nTrls)
+    mkdir(dFldr, "Figures");
+    for u = 1:1 %nTrls
         [pupilSizeTrialU, pupilVids] = pupilAnalysis(dFldr, u, iTOI, cFNames, minX, maxX, minY, maxY);
+        
+%         disp("Trial rpData_pam ", u);
+%         rpData_pam
+%         
+%         
+%         disp("Trial rpData_wholething ", u);
+%         rpData_wholething
+%         
         pupilSizes{u} = pupilSizeTrialU;
         disp("Done with trial");
         disp(u);
     end
     toc
     
-    % Converts the cell array to a Matrix of all values and use Nans to pad
-    [pupilSizesMat] = convertCellArraytoMat(pupilSizes);
-    
-    % Create the dt matrix
-    [pupilSizedt] = creating_dt(pupilSizes);
-    
-    % Filter the dt array
-    [pupilareaProc, pupilsize_filtered] = filterMatrix(pupilSizes, pupilSizedt);
-    
-    % Converts the cell array to a matrix of all values and use Nans to pad
-    [pupilsize_filtered_mat] = convertCellArraytoMat(pupilsize_filtered);
-    
-    % Interpolation
-    [pupilsize_filtered] = interpolation(pupilsize_filtered);
-    
-     % Converts the cell array to a matrix of all values and use Nans to pad
-    [pupilsize_interp_mat] = convertCellArraytoMat(pupilsize_filtered);
+    pupilS = pupilSizes;
+%     % Converts the cell array to a Matrix of all values and use Nans to pad
+%     [pupilSizesMat] = convertCellArraytoMat(pupilSizes);
+%     
+%     % Create the dt matrix
+%     [pupilSizedt] = creating_dt(pupilSizes);
+%     
+%     % Filter the dt array
+%     [pupilareaProc, pupilsize_filtered] = filterMatrix(pupilSizes, pupilSizedt);
+%     
+%     % Converts the cell array to a matrix of all values and use Nans to pad
+%     [pupilsize_filtered_mat] = convertCellArraytoMat(pupilsize_filtered);
+%     
+%     % Interpolation
+%     [pupilsize_filtered] = interpolation(pupilsize_filtered);
+%     
+%      % Converts the cell array to a matrix of all values and use Nans to pad
+%     [pupilsize_interp_mat] = convertCellArraytoMat(pupilsize_filtered);
     
 end
--- EtIBECS/pupilAnalysis.m
+++ EtIBECS/pupilAnalysis.m
@@ -1,58 +1,72 @@
 function [pupilSizes, pupilVids] = pupilAnalysis(dFldr, u, iTOI, cFNames, minX, maxX, minY, maxY)
 %PUPILANALYSIS Summary of this function goes here
 %   Detailed explanation goes here
-    folderName = sprintf("Trial_%d", u);
-    mkdir(dFldr, folderName);
+%     mkdir(dFldr, folderName);
     disp(['Pupil Analysis Trial ', num2str(u)])
     if ~isempty(iTOI)
         % Pupil file info
         pupilFile = strtrim([dFldr cFNames{iTOI}]);
-        pupilInfo = imfinfo(pupilFile);
-        nFrames = size(pupilInfo,1);
-        pupilSizeproc = nan(nFrames,1);
-        pupilVid = nan(maxY-minY+1,maxX-minX+1,nFrames);
-        % Measure pupil
-        area_vector = zeros(1,nFrames);
         
-        for cnt = 1:nFrames
-            %load and crop
-            fullImage = imread(pupilFile,cnt);
-            cropImage=fullImage([minY:maxY], [minX:maxX]);
-            % Threshold
-            skin =~ im2bw(cropImage,0.05);
-            skin = bwmorph(skin,'close');
-            skin = bwmorph(skin,'open');
-            skin = bwareaopen(skin,200);
-            skin = imfill(skin,'holes');
-            % Measure pupil
-            % Select larger area
-            L = bwlabel(skin);
-            [out_a] = regionprops(L);
-            N = size(out_a,1);
-            if N < 1 || isempty(out_a) % Returns if no object in the image
-                solo_cara=[ ];
-                continue
-            end
-            areas=[out_a.Area];
-            [area_max pam]=max(areas);
-            % Measure pupil
-            centro=round(out_a(pam).Centroid);
-            X=centro(1);
-            Y=centro(2);
-            pupilSizeXY = out_a(pam).BoundingBox;
-            sX = pupilSizeXY(3);
-            sY = pupilSizeXY(4);
-            
-            % save data
-            pupilSizeproc(cnt) = mean([sX,sY]);
-            pupilVid(:,:,cnt) = uint8(cropImage);
-            showAllPlot = true;
-            showMeasure = true;
-            % Visualization Stuff
-            visualizePupilAnalysis(folderName, dFldr, showAllPlot, showMeasure, u, cnt, fullImage, minX, maxX, minY, maxY, cropImage, skin, pam, X, Y, sX, sY, out_a);
-        end
-        pupilSizes{u} = pupilSizeproc;
-        pupilVids{u} = pupilVid;
+        
+%         pupilInfo = imfinfo(pupilFile);
+%         nFrames = size(pupilInfo,1);
+%         pupilSizeproc = nan(nFrames,1);
+%         pupilVid = nan(maxY-minY+1,maxX-minX+1,nFrames);
+%         % Measure pupil
+%         area_vector = zeros(1,nFrames);
+%         
+%         for cnt = 1:nFrames
+%             
+%             %load and crop
+%             fullImage = imread(pupilFile,cnt);
+%             cropImage=fullImage([minY:maxY], [minX:maxX]);
+%             % Threshold
+%             skin =~ im2bw(cropImage,0.05);
+%             skin = bwmorph(skin,'close');
+%             skin = bwmorph(skin,'open');
+%             skin = bwareaopen(skin,200);
+%             skin = imfill(skin,'holes');
+%             % Measure pupil
+%             % Select larger area
+%             L = bwlabel(skin);
+%             [out_a] = regionprops(L);
+%             N = size(out_a,1);
+%             if N < 1 || isempty(out_a) % Returns if no object in the image
+%                 solo_cara=[ ];
+%                 continue
+%             end
+%             areas=[out_a.Area];
+%             [area_max pam]=max(areas);
+%             % Measure pupil
+%             centro=round(out_a(pam).Centroid);
+%             X=centro(1);
+%             Y=centro(2);
+% %             majorAxisLength = out_a.MajorAxisLength;
+% %             minorAxisLength = out_a.MinorAxisLength;
+% %             areasOutput = out_a.Area;
+% %             
+% %             rpData_pam = out_a(pam);
+% %             rpData_wholething = out_a;
+% %             
+%             pupilSizeXY = out_a(pam).BoundingBox;
+%             sX = pupilSizeXY(3);
+%             sY = pupilSizeXY(4);
+%             
+%             % save data
+%             pupilSizeproc(cnt) = mean([sX,sY]);
+%             pupilVid(:,:,cnt) = uint8(cropImage);
+%             showAllPlot = true;
+%             showMeasure = true;
+%             % Visualization Stuff
+%             disp(['Pupil Analysis Trial & Frame ', num2str(u), ' ', num2str(cnt)])
+%             visualizePupilAnalysis(u, dFldr, showAllPlot, showMeasure, u, cnt, fullImage, minX, maxX, minY, maxY, cropImage, skin, pam, X, Y, sX, sY, out_a);
+%         end
+%         
+%         pupilSizes{u} = pupilSizeproc;
+%         pupilVids{u} = pupilVid;
+
+        pupilSizes{u} = 1
+        pupilVids{u} = 1
     end
     
     %Plot pupil size over trial   
