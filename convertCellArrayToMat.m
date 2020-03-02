d1ff67701dbcb0a91f4a1da5e60b75a6e7e7d21e
VarunNSrivastava <32204417+VarunNSrivastava@users.noreply.github.com>
2020-03-02 14:41:49 -0800

    Delete convertCellArrayToMat.m

--- convertCellArrayToMat.m
+++
@@ -1,9 +0,0 @@
-function [matrix] = convertCellArrayToMat(cellArray)
-    sizeOfData = size(cellArray);
-    matrix = zeros(sizeOfData(1),sizeOfData(2));
-    for k = 1:sizeOfData(1)
-        for j = 1:sizeOfData(2)
-            matrix(k, j) = cellArray{k,j};
-        end
-    end
-end
